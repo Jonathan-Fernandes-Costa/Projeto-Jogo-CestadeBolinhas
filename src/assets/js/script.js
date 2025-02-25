b54967b000738d57797abffcc224e3bfd30fb42e
Wesley Mesquita <wesleymsqt@alu.ufc.br>
2025-02-25 10:14:34 -0300

    update

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -4,9 +4,7 @@ import * as CANNON from 'cannon-es';
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight); 
-
 document.body.appendChild(renderer.domElement); 
-
 
 const scene = new THREE.Scene();
 
@@ -18,31 +16,31 @@ const camera = new THREE.PerspectiveCamera(
 );
 
 const orbit = new OrbitControls(camera, renderer.domElement);
-
 camera.position.set(4, 15, 13); 
 orbit.update(); 
 
-const ambientLight = new THREE.AmbientLight(0x333333);
+const ambientLight = new THREE.AmbientLight(0x333333); // Corrigido
 scene.add(ambientLight);
 
-const directionalLight = new THREE.DirectionalLight(0xFFFFF, 0.8);
+const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 0.8); // Corrigido
 scene.add(directionalLight);
 directionalLight.position.set(0, 50, 0);
 
-// const helper = new THREE.AxesHelper(20);
-// scene.add(helper);
-
-const world = new CANNON.World({gravity: new CANNON.Vec3(0, -9.81, 0)});
+const world = new CANNON.World({ gravity: new CANNON.Vec3(0, -9.81, 0) });
 
 const planeGeo = new THREE.PlaneGeometry(10, 10);
 const planeMat = new THREE.MeshStandardMaterial({
     color: 0xFFFFFF,
     side: THREE.DoubleSide
 });
+const planeMesh = new THREE.Mesh(planeGeo, planeMat);
+scene.add(planeMesh);
 
+const planePhysMat = new CANNON.Material();
 const planeBody = new CANNON.Body({
     type: CANNON.Body.STATIC,
-    shape: new CANNON.Box(new CANNON.Vec3(3, 5, 0.001))
+    shape: new CANNON.Box(new CANNON.Vec3(5, 5, 0.001)),
+    material: planePhysMat
 });
 planeBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
 world.addBody(planeBody);
@@ -53,9 +51,9 @@ const planeNormal = new THREE.Vector3();
 const plane = new THREE.Plane();
 const raycaster = new THREE.Raycaster();
 
-window.addEventListener('mousemove', function(e) {
+window.addEventListener('mousemove', function (e) {
     mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
-    mouse.y = (e.clientY / window.innerWidth) * 2 + 1;
+    mouse.y = -(e.clientY / window.innerHeight) * 2 + 1; // Corrigido
     planeNormal.copy(camera.position).normalize();
     plane.setFromNormalAndCoplanarPoint(planeNormal, scene.position);
     raycaster.setFromCamera(mouse, camera);
@@ -65,22 +63,32 @@ window.addEventListener('mousemove', function(e) {
 const meshes = [];
 const bodies = [];
 
-window.addEventListener('click', function(e) {
+window.addEventListener('click', function (e) {
     const sphereGeo = new THREE.SphereGeometry(0.125, 30, 30);
     const sphereMat = new THREE.MeshStandardMaterial({
-        color: 0xFFEA00,
+        color: Math.random() * 0xFFFFFF,
         metalness: 0,
         roughness: 0
     });
     const sphereMesh = new THREE.Mesh(sphereGeo, sphereMat);
-    scene.add(sphereMat);
-    sphereMesh.position.copy(intersectionPoint);
+    scene.add(sphereMesh); // Corrigido
+    sphereMesh.position.copy(intersectionPoint); // Descomentado
 
-    const sphereBody = new CANNON.body({
+    const spherePhysMat = new CANNON.Material();
+    const sphereBody = new CANNON.Body({
         mass: 0.3,
-        sape: new CANNON.Sphere(0.125)
+        shape: new CANNON.Sphere(0.125),
+        position: new CANNON.Vec3(intersectionPoint.x, intersectionPoint.y, intersectionPoint.z),
+        material: spherePhysMat
     });
     world.addBody(sphereBody);
+
+    const planeSphereContactMat = new CANNON.ContactMaterial(
+        planePhysMat,
+        spherePhysMat,
+        { restitution: 0.3 }
+    );
+    world.addContactMaterial(planeSphereContactMat);
 
     meshes.push(sphereMesh);
     bodies.push(sphereBody);
@@ -88,14 +96,13 @@ window.addEventListener('click', function(e) {
 
 const timestep = 1 / 60;
 
-
-function animate(){
+function animate() {
     world.step(timestep);
 
     planeMesh.position.copy(planeBody.position);
     planeMesh.quaternion.copy(planeBody.quaternion);
 
-    for(let i = 0; i < meshes.length; i++) {
+    for (let i = 0; i < meshes.length; i++) {
         meshes[i].position.copy(bodies[i].position);
         meshes[i].quaternion.copy(bodies[i].quaternion);
     }
@@ -106,8 +113,7 @@ function animate(){
 renderer.setAnimationLoop(animate);
 
 window.addEventListener('resize', function () {
-    camera.aspect = this.window.innerWidth / window.innerHeight;
+    camera.aspect = window.innerWidth / window.innerHeight; // Corrigido
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
-})
-
+});
